06b654dfb2241f7095f5113da7ad61ac59aedf64
Ahrusy <your.email@example.com>
2025-07-13 23:25:03 +0300

    add new code

--- myproject/pages/urls.py
+++ myproject/pages/urls.py
@@ -1,7 +1,12 @@
+# myproject/pages/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('data/', views.data_page, name='data'),
     path('test/', views.test_page, name='test'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
--- myproject/pages/views.py
+++ myproject/pages/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+# myproject/pages/views.py
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
 
 def data_page(request):
     return render(request, 'pages/data.html')
